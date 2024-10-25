1f9bd30b1d40de16d00abc52674b3c350f698e64
DeeshaPatel <pateldeesha99@gmail.com>
2024-10-25 14:37:15 -0400

    Delete code_build directory

--- code_build/3-outputs.tf
+++
@@ -1,7 +0,0 @@
-output "codebuild_project_name" {
-  value = aws_codebuild_project.codebuild_project_template.name
-}
-
-output "build_assume_role" {
-  value = aws_iam_role.iam_codebuild_assume_role.id
-}
